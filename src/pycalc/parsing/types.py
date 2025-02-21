105e1e4ae17a91a6f8ac2af58cdc3c9cd87b5380
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2025-02-21 15:22:38 +0500

    feat: Add basic instructions.

---
+++ src/pycalc/parsing/types.py
@@ -0,0 +1,73 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+from typing import List
+
+
+class OperationType(Enum):
+    ADDITION = 1
+    SUBTRACTION = 2
+    MULTIPLICATION = 3
+    DIVISION = 4
+    EQUAL = 5
+    NOT_EQUAL = 6
+
+
+class ConstantType(Enum):
+    INT = 1
+    FLOAT = 2
+
+
+class Instruction(ABC):
+    """Base instruction class."""
+
+
+class Program(Instruction):
+    """Program instruction class."""
+
+    def __init__(self, instruction: Instruction):
+        self.instructions = instruction
+
+
+class Constant(Instruction):
+    """Constant instruction class."""
+
+    def __init__(
+        self,
+        value: str,
+        constant_type
+    ):
+        self.value = value
+        self.constant_type = constant_type
+
+
+class IdentifierInstruction(Instruction):
+    """Identifier instruction class."""
+
+    def __init__(self, identifier: str):
+        self.identifier = identifier
+
+
+class BinaryOperation(Instruction):
+    """Binary operation class."""
+
+    def __init__(
+        self,
+        left: Instruction,
+        right: Instruction,
+        op_type: OperationType,
+    ):
+        self.left = left
+        self.right = right
+        self.op_type = op_type
+
+
+class UnaryOperation(Instruction):
+    """Represents unary operation."""
+
+    def __init__(
+        self,
+        op_type: OperationType,
+        operand: Instruction,
+    ):
+        self.op_type = op_type
+        self.operand = operand
