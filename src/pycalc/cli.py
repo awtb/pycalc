f89c1aef53a43d4dd3940b41942da741d7401955
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2025-02-22 15:46:50 +0500

    feat: Add `cli.py`

---
+++ src/pycalc/cli.py
@@ -0,0 +1,27 @@
+from argparse import ArgumentParser
+
+from pycalc.calculator import calc
+
+def build_argument_parser() -> ArgumentParser:
+    parser = ArgumentParser(
+        prog="pycalc", description="A simple calculator", epilog="Help"
+    )
+
+    parser.add_argument("--expression", type=str, required=False)
+
+    return parser
+
+
+def main():
+    parser = build_argument_parser()
+
+    args = parser.parse_args()
+
+    if args.expression:
+        print("Result:", calc(expression=args.expression))
+        return
+
+    while True:
+        expr = input("> ")
+
+        print("Result:", calc(expr))
