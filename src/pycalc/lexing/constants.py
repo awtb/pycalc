5c90932e4b589c3f7a091c4f0f9d263bdab9d54e
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2025-02-22 15:46:05 +0500

    refactor: Reformatted the code.

--- src/pycalc/lexing/constants.py
+++ src/pycalc/lexing/constants.py
@@ -1,11 +1,11 @@
 from src.pycalc.lexing.types import TokenType
 
 CHARS_TOKEN_MAPPING = {
-    '*': TokenType.MUL,
-    '+': TokenType.PLUS,
-    '-': TokenType.MINUS,
-    '/': TokenType.DIV,
-    '(': TokenType.LPAREN,
-    ')': TokenType.RPAREN,
+    "*": TokenType.MUL,
+    "+": TokenType.PLUS,
+    "-": TokenType.MINUS,
+    "/": TokenType.DIV,
+    "(": TokenType.LPAREN,
+    ")": TokenType.RPAREN,
     "^": TokenType.POW,
 }
