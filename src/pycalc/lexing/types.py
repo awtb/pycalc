8e02481063be77abd2a33822ee48bb006282d051
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2025-02-23 13:37:10 +0500

    feat: Add `lineno` and `character` to `Token`

--- src/pycalc/lexing/types.py
+++ src/pycalc/lexing/types.py
@@ -15,9 +15,11 @@ class TokenType(StrEnum):
 
 
 class Token:
-    def __init__(self, token_type, value):
+    def __init__(self, token_type: TokenType, value: str, lineno: int, character: int):
         self.type = token_type
         self.value = value
+        self.lineno = lineno
+        self.character = character
 
     def __str__(self):
         return f"{self.type}: {self.value}"
